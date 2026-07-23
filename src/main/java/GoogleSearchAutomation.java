85e7f887535a6446e2ca7730d60319c8c964f8d1
727724euit297 <727724euit297@skcet.ac.in>
2026-07-23 19:47:08 +0530

    Added Google Search Automation using Selenium

---
+++ src/main/java/GoogleSearchAutomation.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearchAutomation {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.com");
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+
+        searchBox.sendKeys("What is Java");
+
+        searchBox.submit();
+
+        System.out.println("Page Title: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
